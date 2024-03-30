118b4c9e73733ee67fc316acf1b6f4150b87cca4
Serge <dingreville-s@live.fr>
2024-03-30 14:11:57 +0100

    affichage produits / pages produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,25 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
 
-    #[Route('/product/category')]
-    public function category ()
+    #[Route('/productlist')]
+    public function productList (ProductRepository $productRepository)
     {
-        return $this -> render ('product/category.html.twig');
+        $products = $productRepository->findAll();
+
+        return $this->render('product/productList.html.twig', [
+            'products' => $products
+        ]);
     }
 
-    #[Route('/product/productlist')]
-    public function productList ()
+    #[Route('/productcategory/{id}')]
+    public function productCategory($id, CategoryRepository $categoryRepository)
     {
-        return $this -> render ('product/productList.html.twig');
-    }#[Route('/product/productpage')]
-    public function productPage ()
-    {
-        return $this -> render ('product/productPage.html.twig');
+        $category = $categoryRepository->find($id);
+        return $this -> render('product/productCategory.html.twig',[
+            'category' => $category
+        ]);
     }
+
+    #[Route('/productdetails/{name}')]
+    public function productDetails($name, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($name);
+        return $this -> render('product/productDetails.html.twig',[
+            'product' => $product
+        ]);
+    }
+
 }
